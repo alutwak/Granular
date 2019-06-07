532d38b6536fd687eae1cf57a452df4f1eed4416
Ayal Lutwak <worm60@gmail.com>
2019-06-06 23:15:24 -0700

    Combined Wavetable into Waveform and made it a final class

--- grain/waveform.cpp
+++ grain/waveform.cpp
@@ -1,15 +1,179 @@
 
 #include <cmath>
+#include <cstring>
 #include "waveform.hpp"
+#include "phasor.hpp"
 
 namespace audioelectric {
   
   /*********************** Public Waveform *******************************/
 
   template<typename T>
-  Waveform<T>::~Waveform(void) {}
+  T doInterpolate(const T* a, double offset, InterpType it);
   
+  /*********************** Public Waveform *******************************/
+
+  template<typename T>
+  Waveform<T>::Waveform(void) : _interptype(InterpType::LINEAR), _data(nullptr), _size(0)
+  {
+    
+  }
+
+  template<typename T>
+  Waveform<T>::Waveform(std::size_t len, InterpType it) : _interptype(it), _data(nullptr), _size(0)
+  {
+    alloc(len);
+    memset(_data, 0, sizeof(T)*len);
+  }
+  
+  template<typename T>
+  Waveform<T>::Waveform(T* data, std::size_t len, InterpType it) : _interptype(it), _data(nullptr), _size(len)
+  {
+    alloc(len);
+    memcpy(_data, data, sizeof(T)*len);
+  }
+
+  template<typename T>
+  Waveform<T>::Waveform(std::initializer_list<T> init, InterpType it) : _interptype(it), _data(nullptr), _size(0)
+  {
+    alloc(init.size());
+    T* p = _data;
+    for (auto& val : init)
+      *(p++) = val;
+  }
+
+  template<typename T>
+  Waveform<T>::Waveform(Waveform<T>& other, double rate, std::size_t len, InterpType it) :
+    _interptype(it), _data(nullptr), _size(0)
+  {
+    alloc(len);
+    auto phs = Phasor<T>(other, rate);
+    T* p = _data;
+    phs.generate(&_data, len, 1);
+  }
+
+  template<typename T>
+  Waveform<T>::~Waveform(void)
+  {
+    dealloc();
+  }
+
+  template<typename T>
+  T Waveform<T>::waveform(double pos, int channel)
+  {
+    if (pos < 0 || pos > _size-1)
+      return 0;
+    return interpLinear(pos);
+  }
+  
+  template<typename T>
+  Waveform<T>& Waveform<T>::operator=(const Waveform<T>& other)
+  {
+    alloc(other._size);
+    _interptype = other._interptype;
+    memcpy(_data,other._data,sizeof(T)*_size);
+    return *this;
+  }
+
+
+  template<typename T>
+  typename Waveform<T>::iterator Waveform<T>::ibegin(void)
+  {
+    return iterator(_data);
+  }
+
+  template<typename T>
+  typename Waveform<T>::iterator Waveform<T>::iend(void)
+  {
+    return iterator(_data+_size);
+  }
+
+  template<typename T>
+  void Waveform<T>::resize(std::size_t len) {
+    if (len == _size)
+      return;
+    alloc(len);
+  }
+  
+  /*********************** Private Waveform *******************************/
+
+  template<typename T>
+  void Waveform<T>::alloc(std::size_t len)
+  {
+    dealloc();
+    _data = new T[len];
+    _size = len;
+  }
+
+  template<typename T>
+  void Waveform<T>::dealloc(void)
+  {
+    if (_data)
+      delete[] _data;
+    _size = 0;
+  }
+
+  template<typename T>
+  T Waveform<T>::interpLinear(double pos) const
+  {
+    long p = pos;
+    T a = _data[p];
+    T b = _data[p+1];
+    double diff = fmod(pos,1.0);
+    return (b-a)*diff + a;
+  }
+  
+  /********************* iterator ********************/
+  
+  template<typename T>
+  typename Waveform<T>::iterator& Waveform<T>::iterator::operator++(void)
+  {
+    _data++;
+    return *this;
+  }
+
+  template<typename T>
+  typename Waveform<T>::iterator Waveform<T>::iterator::operator++(int)
+  {
+    iterator temp = *this;
+    _data++;
+    return temp;
+  }
+
+  template<typename T>
+  T& Waveform<T>::iterator::operator*(void)
+  {
+    return *_data;
+  }
+
+  template<typename T>
+  bool Waveform<T>::iterator::operator==(const Waveform<T>::iterator& other) const
+  {
+    return _data == other._data;
+  }
+  
+  template<typename T>
+  bool Waveform<T>::iterator::operator!=(const Waveform<T>::iterator& other) const
+  {
+    return _data != other._data;
+  }
+
   template class Waveform<double>;
   template class Waveform<float>;
 
+  template<typename T>
+  void GenerateGaussian(Waveform<T> *wt, std::size_t len, T sigma) {
+    wt->resize(len);
+    T flen = len;
+    T mid = -flen/2;
+    T sigma_norm = -2*(sigma*flen)*(sigma*flen);
+    T offset = exp(mid*mid/sigma_norm);
+    T norm = 1.0/(1.0-offset);
+    T* data = wt->data();
+    for (std::size_t i=0; i<len; i++) {
+      mid += 1.0;
+      data[i] = norm*(exp(mid*mid/sigma_norm)-offset);
+    }
+  }
+  
 }
--- grain/waveform.hpp
+++ grain/waveform.hpp
@@ -2,40 +2,141 @@
 #pragma once
 
 #include <cstdlib>
+#include <initializer_list>
 
 namespace audioelectric {
 
-  /*!\brief Provides an iterface for a variety of waveforms. These could be waveforms that get generated on the fly
-   * or wavetables, samples, envelopes, etc...
-   *
-   * \note Waveform has been purposely left as a single-channel base class in order to keep the overhead of multiple channels
-   * out of sublasses that only need one channel (like Grain, envelope or probably a handful of other potential waveforms
-   * that are used for their waveform properties rather than their pure audio properties). It should be pretty easy for
-   * subclasses to add channels though. You'll just need to make separate phasors for each channel. The more interesting
-   * challenge will be fixing operator[] so that you can use syntax like sample[chan][pos].  That's not strictly necessary,
-   * but it would be a nice touch and make indexing multi-channel waveforms much easier.
+  /*!\brief Different types of interpolation
+   */
+  enum class InterpType {
+    LINEAR,     //!< Linear interpolation
+  };
+
+  /*!\brief Contains and manages a set of audio data. Useful for samples, grains or any other chunk of audio data that needs
+   * needs to be stored and manipulated.
    */
   template<typename T>
-  class Waveform {
+  class Waveform final {
   public:
 
-    virtual ~Waveform(void) = 0;
+    /*!\brief A common iterator for the Waveform. This works just like that of a vector's iterator
+     */
+    class iterator {
+    public:
+      iterator(T* data) : _data(data) {};
+      iterator(const iterator& other) : _data(other._data) {};
+      iterator& operator++(void);
+      iterator operator++(int);
+      T& operator*(void);
+      bool operator==(const iterator& other) const;
+      bool operator!=(const iterator& other) const;      
+    private:
+      T* _data;
+    };
+
+    /*!\brief Creates a waveform of size 0
+     */
+    Waveform(void);
+
+    /*!\brief Creates and allocates a waveform of size len with all values set to 0
+     */
+    Waveform(std::size_t len, InterpType it=InterpType::LINEAR);
+
+    /*!\brief Wraps an array of size len in a Waveform to allow it to be interpolated
+     */
+    Waveform(T* data, std::size_t len, InterpType it=InterpType::LINEAR);
+
+    Waveform(std::initializer_list<T> init, InterpType it=InterpType::LINEAR);
     
-    /*!\brief Returns the waveform at a certain phase
+    /*!\brief Copies a sample to a new length using interpolation
+     */
+    Waveform(Waveform<T>& other, double rate, std::size_t len, InterpType it=InterpType::LINEAR);
+
+    ~Waveform(void);
+
+    /*!\brief Sets the interpolation type
+     */
+    void setInterpType(InterpType it) {_interptype = it;}
+
+    /*!\brief Returns the current interpolation type
+     */
+    InterpType getInterpType(void) const {return _interptype;}
+
+    /*!\brief Returns the interpolated value at a position in the waveform.
      * 
-     * \param pos The position of the waveform
-     * \return The waveform at the given phase
+     * The position is a generalized index of the samples in the waveform, such that an integer position will return 
+     * the same value as that returned by Waveform[], and a non-integer position will return an interpolated value as though
+     * the waveform were continuous. 
+     * 
+     * Positions outside of the waveform -- those less than 0 and greater than the size of the waveform (in samples) -- will 
+     * return a value of 0. This is done so that a Waveform can be easily mixed with other Waveforms without having to worry
+     * about bounds. 
+     * 
+     * \param pos The position on the waveform
+     * \param channel The channel to retrieve from (ignored for now)
+     * \return The interpolated value at pos
      */
-    virtual T waveform(double pos, int channel=0) = 0;
+    T waveform(double pos, int channel=0);
 
-    /*!\brief Returns the number of samples of the waveform
-     */
-    virtual std::size_t size(void) const = 0;
+    Waveform<T>& operator=(const Waveform<T>& other);
 
-    /*!\brief Returns the end of the waveform
+    T& operator[](std::size_t pos) {return _data[pos];}
+    const T& operator[](std::size_t pos) const {return _data[pos];}
+
+    iterator ibegin(void);
+    iterator iend(void);
+    
+    /*!\brief Returns the number of samples in the Waveform
      */
-    virtual double end(void) const = 0;
+    std::size_t size(void) const {return _size;}
+
+    /*!\brief Returns the end sample of the Waveform
+     *
+     */
+    double end(void) const {return _size-1;}
+
+    /*!\brief Resizes the Waveform. All data is cleared
+     */
+    void resize(std::size_t len);
+
+    /*!\brief Returns a pointer to the raw data
+     */
+    T* data(void) {return _data;}
+    
+  private:
+
+    InterpType _interptype;     //!< The interpolation type
+    long _size;                 //!< The size of _data
+    T* _data;                   //!< The raw data
+
+    /*!\brief Allocates a data array of length len
+     */
+    void alloc(std::size_t len);
+
+    /*!\brief Deallocates _data
+     */
+    void dealloc(void);
+
+    /*!\brief Performs a linear interpolation of the point at pos
+     *
+     * If pos is <0 or >_size-1, this will always return 0
+     */
+    T interpLinear(double pos) const;
+    
 
   };
+
+  /*!\brief Generates a gaussian function and puts it in a wavetable
+   * 
+   * The generated function is: r*(e^(-(x-len/2)^2/sigma)-y), where y is the value of the normal Gaussian at the 
+   * beginning and end of the signal and r renormalizes the signal after that subtraction. This ensures that the signal
+   * begins and ends with a value of zero.
+   *
+   * \param[in,out] wt The Wavetable to fill (this will overwrite any function already in it)
+   * \param len The length of the gaussian function
+   * \param sigma The variance of the gaussian
+   */
+  template<typename T>
+  void GenerateGaussian(Waveform<T> *wt, std::size_t len, T sigma);
 
 }
