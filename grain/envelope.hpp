a5ea78dda2130e38f2313bbb25d0b3923e36fdae
Ayal Lutwak <worm60@gmail.com>
2019-07-21 23:26:08 -0700

    Corrected envelope.value() return type

--- grain/envelope.hpp
+++ grain/envelope.hpp
@@ -33,7 +33,7 @@ namespace audioelectric {
      * 
      * This value is undefined if operator bool returns false
      */
-    double value(void) const {return _out;}
+    T value(void) const {return _out;}
 
     void increment(void);
 
